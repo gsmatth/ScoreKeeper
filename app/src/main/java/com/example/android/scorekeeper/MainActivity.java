68cd37b6fb59bb10c1d5d28ea72e35a2fe84453b
Doug Popadince <gsmatth@icloud.com>
2017-02-22 15:22:45 -0700

    completed styling

--- app/src/main/java/com/example/android/scorekeeper/MainActivity.java
+++ app/src/main/java/com/example/android/scorekeeper/MainActivity.java
@@ -19,32 +19,28 @@ public class MainActivity extends AppCompatActivity {
         TextView scoreView = (TextView) findViewById(R.id.team_a_score);
         scoreView.setText(String.valueOf(score));
     }
-    //touchdown
+
     public void addTouchdownForTeamA(View view) {
         scoreTeamA += 6;
         displayScoreForTeamA(scoreTeamA);
     }
 
 
-    //extrapoint
     public void addExtraPointForTeamA(View view) {
         scoreTeamA += 1;
         displayScoreForTeamA(scoreTeamA);
     }
 
-    //conversion
     public void addConversionForTeamA(View view) {
         scoreTeamA += 2;
         displayScoreForTeamA(scoreTeamA);
     }
 
-    //fieldgoal
     public void addFieldGoalForTeamA(View view) {
         scoreTeamA += 3;
         displayScoreForTeamA(scoreTeamA);
     }
 
-    //safety
     public void addSafetyForTeamA(View view) {
         scoreTeamA += 2;
         displayScoreForTeamA(scoreTeamA);
@@ -55,39 +51,34 @@ public class MainActivity extends AppCompatActivity {
         TextView scoreView = (TextView) findViewById(R.id.team_b_score);
         scoreView.setText(String.valueOf(score));
     }
-    //touchdown
+
     public void addTouchdownForTeamB(View view) {
         scoreTeamB += 6;
         displayScoreForTeamB(scoreTeamB);
     }
 
 
-    //extrapoint
     public void addExtraPointForTeamB(View view) {
         scoreTeamB += 1;
         displayScoreForTeamB(scoreTeamB);
     }
 
-    //conversion
     public void addConversionForTeamB(View view) {
         scoreTeamB += 2;
         displayScoreForTeamB(scoreTeamB);
     }
 
-    //fieldgoal
     public void addFieldGoalForTeamB(View view) {
         scoreTeamB += 3;
         displayScoreForTeamB(scoreTeamB);
     }
 
-    //safety
     public void addSafetyForTeamB(View view) {
         scoreTeamB += 2;
         displayScoreForTeamB(scoreTeamB);
     }
 
-    //reset
-    public void resetBothTeamScores(View view){
+    public void resetBothTeamScores(View view) {
         scoreTeamA = 0;
         scoreTeamB = 0;
         displayScoreForTeamA(scoreTeamA);
